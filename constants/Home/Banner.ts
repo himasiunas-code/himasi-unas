3b2f23ee23313a07c6107d2c52dd0bc1a9096ba0
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-10 21:08:44 +0700

    refactor: comment out unused banner data in Banner component

--- constants/Home/Banner.ts
+++ constants/Home/Banner.ts
@@ -1,52 +1,12 @@
 import { TBanner } from "@/lib/type/Home/tBanner";
 
 export const BANNER_DATA: TBanner[] = [
-  {
-    id: 1,
-    title: "Kegiatan Akademik",
-    description:
-      "Berbagai kegiatan untuk meningkatkan kompetensi akademik mahasiswa Sistem Informasi",
-    image: "/image/Home/Banner 1.png",
-    alt: "Banner Kegiatan Akademik HIMASI",
-  },
-  {
-    id: 2,
-    title: "Kegiatan Sosial",
-    description:
-      "Program pengabdian masyarakat dan kegiatan sosial untuk membangun karakter mahasiswa",
-    image: "/image/Home/Banner 2.png",
-    alt: "Banner Kegiatan Sosial HIMASI",
-  },
-  {
-    id: 3,
-    title: "Kegiatan Organisasi",
-    description:
-      "Kegiatan internal organisasi untuk mempererat hubungan antar anggota HIMASI",
-    image: "/image/Home/Banner 3.png",
-    alt: "Banner Kegiatan Organisasi HIMASI",
-  },
-  {
-    id: 4,
-    title: "Kegiatan Akademik",
-    description:
-      "Berbagai kegiatan untuk meningkatkan kompetensi akademik mahasiswa Sistem Informasi",
-    image: "/image/Home/Banner 1.png",
-    alt: "Banner Kegiatan Akademik HIMASI",
-  },
-  {
-    id: 5,
-    title: "Kegiatan Sosial",
-    description:
-      "Program pengabdian masyarakat dan kegiatan sosial untuk membangun karakter mahasiswa",
-    image: "/image/Home/Banner 2.png",
-    alt: "Banner Kegiatan Sosial HIMASI",
-  },
-  {
-    id: 6,
-    title: "Kegiatan Organisasi",
-    description:
-      "Kegiatan internal organisasi untuk mempererat hubungan antar anggota HIMASI",
-    image: "/image/Home/Banner 3.png",
-    alt: "Banner Kegiatan Organisasi HIMASI",
-  },
+  // {
+  //   id: 1,
+  //   title: "Kegiatan Akademik",
+  //   description:
+  //     "Berbagai kegiatan untuk meningkatkan kompetensi akademik mahasiswa Sistem Informasi",
+  //   image: "/image/Home/Banner 1.png",
+  //   alt: "Banner Kegiatan Akademik HIMASI",
+  // },
 ];
