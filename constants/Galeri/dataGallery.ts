593cdf3ff65c50dfea424ecf942d8d218e0e66de
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-10 21:08:02 +0700

    refactor: update gallery images and remove unused banner data

--- constants/Galeri/dataGallery.ts
+++ constants/Galeri/dataGallery.ts
@@ -2,28 +2,28 @@ import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri/Galeri";
 
 export const galleryImages: GalleryImage[] = [
   {
-    src: "/image/Home/Banner 1.png",
-    alt: "Kegiatan HIMASI 1",
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 1",
   },
   {
-    src: "/image/Home/Banner 2.png",
-    alt: "Kegiatan HIMASI 2",
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 2",
   },
   {
-    src: "/image/Home/Banner 3.png",
-    alt: "Kegiatan HIMASI 3",
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 3",
   },
   {
-    src: "/image/Home/Kahim.png",
-    alt: "Kegiatan HIMASI 4"
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 4"
   },
   {
-    src: "/image/Home/Kahims.png",
-    alt: "Kegiatan HIMASI 5",
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 5",
   },
   {
-    src: "/image/Struktur/Deadpool.png",
-    alt: "Kegiatan HIMASI 6",
+    src: "/image/Galeri/Himpunan/h-2.jpg",
+    alt: "HIMASI 6",
   },
 ];
 
