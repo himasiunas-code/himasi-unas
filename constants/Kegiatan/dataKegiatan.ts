b0f94b6d2cdc396de61256da47ad236385ca4c29
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-10 21:09:17 +0700

    refactor: comment out unused kegiatan data in dataKegiatan.ts

--- constants/Kegiatan/dataKegiatan.ts
+++ constants/Kegiatan/dataKegiatan.ts
@@ -9,40 +9,13 @@ export interface KegiatanData {
 }
 
 export const kegiatanData: KegiatanData[] = [
-  {
-    id: 1,
-    title: "KEGIATAN 1",
-    subtitle: "Seminar Teknologi Informasi & Workshop Programming",
-    date: "01-01-2025",
-    description: "Loremuah ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat facilisis sem vel semper. Ut congue ante quis quam vestibulum, ac scelerisque elit semper. Nulla nec ex eget ante bibendum lobortis.",
-    image: "/image/Home/Banner 1.png",
-    buttonText: "Baca"
-  },
-  {
-    id: 2,
-    title: "KEGIATAN 2",
-    subtitle: "Kompetisi Coding & Hackathon Mahasiswa",
-    date: "01-01-2025",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat facilisis sem vel semper. Ut congue ante quis quam vestibulum, ac scelerisque elit semper. Nulla nec ex eget ante bibendum lobortis.",
-    image: "/image/Kegiatan/kegiatan2.jpg",
-    buttonText: "Baca"
-  },
-  {
-    id: 3,
-    title: "KEGIATAN 3",
-    subtitle: "Study Excursion & Company Visit",
-    date: "01-01-2025", 
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat facilisis sem vel semper. Ut congue ante quis quam vestibulum, ac scelerisque elit semper. Nulla nec ex eget ante bibendum lobortis.",
-    image: "/image/Kegiatan/kegiatan3.jpg",
-    buttonText: "Baca"
-  },
-  {
-    id: 4,
-    title: "KEGIATAN 4",
-    subtitle: "Pelatihan Soft Skills & Leadership Training",
-    date: "01-01-2025",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat facilisis sem vel semper. Ut congue ante quis quam vestibulum, ac scelerisque elit semper. Nulla nec ex eget ante bibendum lobortis.",
-    image: "/image/Kegiatan/kegiatan4.jpg", 
-    buttonText: "Baca"
-  }
+  // {
+  //   id: 1,
+  //   title: "KEGIATAN 1",
+  //   subtitle: "Seminar Teknologi Informasi & Workshop Programming",
+  //   date: "01-01-2025",
+  //   description: "Loremuah ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat facilisis sem vel semper. Ut congue ante quis quam vestibulum, ac scelerisque elit semper. Nulla nec ex eget ante bibendum lobortis.",
+  //   image: "/image/Home/Banner 1.png",
+  //   buttonText: "Baca"
+  // },
 ];
