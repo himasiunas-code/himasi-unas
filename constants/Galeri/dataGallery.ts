6cf0835d0444f0d086acff64b44ee32c0d098cd7
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-19 22:17:18 +0700

    fix: update image file extensions in galleryImages to match correct casing

--- constants/Galeri/dataGallery.ts
+++ constants/Galeri/dataGallery.ts
@@ -2,27 +2,27 @@ import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri/Galeri";
 
 export const galleryImages: GalleryImage[] = [
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 1",
   },
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 2",
   },
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 3",
   },
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 4"
   },
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 5",
   },
   {
-    src: "/image/Galeri/Himpunan/h-2.jpg",
+    src: "/image/Galeri/Himpunan/h-2.JPG",
     alt: "HIMASI 6",
   },
 ];
