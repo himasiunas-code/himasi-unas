2eb670bd617ea32e6361e21055e2c8111af833d8
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-11-06 19:07:32 +0700

    fix: update logo import path to use HIMASI.png for consistency

--- components/shared/Home/Hero.tsx
+++ components/shared/Home/Hero.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Banner from "@/public/image/Home/BannerV1.png";
-import Logo from "@/public/icon/image.png";
+import Logo from "@/public/icon/HIMASI.png";
 
 export default function Hero() {
   return (
