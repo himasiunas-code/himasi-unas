78a27800ecdc93b087b6476a18fd1b8574795769
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-12-05 01:36:20 +0700

    fix: update slide images to use correct sources

--- constants/Home/SlideImages.ts
+++ constants/Home/SlideImages.ts
@@ -1,45 +1,50 @@
-import KahimImage from "@/public/image/Home/Kahim.png";
+import BPH from "@/public/image/Home/BPH.png";
+import RND from "@/public/image/Home/RND.png";
+import PR from "@/public/image/Home/PR.png";
+import CM from "@/public/image/Home/CM.png";
+import ETR from "@/public/image/Home/ETR.png";
+import HTD from "@/public/image/Home/HTD.png";
 import { SlideImageData } from "@/lib/type/Home/tSlideImage";
 
 export const slideImages: SlideImageData[] = [
   {
     id: 1,
-    src: KahimImage,
+    src: BPH,
     alt: "HIMASI Keren",
     backgroundColor: "#ffffffff",
     textColor: "white",
   },
   {
     id: 2,
-    src: KahimImage,
+    src: RND,
     alt: "HIMASI Hebat",
     backgroundColor: "#ffffffff",
     textColor: "white",
   },
   {
     id: 3,
-    src: KahimImage,
+    src: PR,
     alt: "HIMASI Kuat",
     backgroundColor: "#ffffffff",
     textColor: "black",
   },
   {
     id: 4,
-    src: KahimImage,
+    src: CM,
     alt: "HIMASI Kreatif",
     backgroundColor: "#ffffffff",
     textColor: "black",
   },
   {
     id: 5,
-    src: KahimImage,
+    src: ETR,
     alt: "HIMASI Ramah",
     backgroundColor: "#ffffffff",
     textColor: "white",
   },
   {
     id: 6,
-    src: KahimImage,
+    src: HTD,
     alt: "HIMASI UNAS",
     backgroundColor: "#ffffffff",
     textColor: "white",
