7ade5ecc581c384621eb5a5ece7f32306657cec6
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-12-30 19:54:15 +0700

    feat: update BANNER_DATA to include Workshop details

--- constants/Home/Banner.ts
+++ constants/Home/Banner.ts
@@ -1,12 +1,12 @@
 import { TBanner } from "@/lib/type/Home/tBanner";
 
 export const BANNER_DATA: TBanner[] = [
-  // {
-  //   id: 1,
-  //   title: "Kegiatan Akademik",
-  //   description:
-  //     "Berbagai kegiatan untuk meningkatkan kompetensi akademik mahasiswa Sistem Informasi",
-  //   image: "/image/Home/Banner 1.png",
-  //   alt: "Banner Kegiatan Akademik HIMASI",
-  // },
+  {
+    id: 1,
+    title: "Workshop",
+    description:
+      "Workshop Game Developer untuk meningkatkan keterampilan dalam pengembangan game.",
+    image: "image/Home/Kegiatan/workshop-game-developer.jpg",
+    alt: "Workshop Game Developer HIMASI UNAS",
+  },
 ];
