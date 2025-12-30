30b40c50f48cbdc3e501f064cd202e6865903dfa
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-12-30 19:54:08 +0700

    feat: add filter categories and descriptions for Galeri HIMASI 2024

---
+++ constants/Galeri/filterGallery2024.ts
@@ -0,0 +1,24 @@
+import { GalleryFilter } from "@/lib/type/Galeri/Galeri";
+
+// Filter categories
+export const galleryFilters2024: GalleryFilter[] = [
+  "All",
+  "PLBA",
+  "Himpunan",
+];
+
+// Filter display names and descriptions
+export const filterDescriptions2024: Record<
+  GalleryFilter,
+  { displayName: string; description: string }
+> = {
+  All: { displayName: "All", description: "Semua Foto Kegiatan 2024" },
+  "PLBA": {
+    displayName: "PLBA",
+    description: "Pengenalan Lingkungan dan Budaya Akademik 2024",
+  },
+  Himpunan: {
+    displayName: "Himpunan",
+    description: "Kebersamaan HIMASI 2024",
+  },
+};
