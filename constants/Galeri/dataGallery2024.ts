c141a74159984487af4d6c6f27a378efcf0a550e
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-12-30 19:54:00 +0700

    feat: add initial data structure for Galeri HIMASI 2024

---
+++ constants/Galeri/dataGallery2024.ts
@@ -0,0 +1,16 @@
+import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri/Galeri";
+
+export const galleryImages2024: GalleryImage[] = [
+//   {
+//     src: "/Coming.jpg",
+//     alt: "Kegiatan HIMASI 2024 - 1",
+//     category: "PLBA",
+//   },
+];
+
+export const galleryEvent2024: GalleryEvent = {
+  path: "galeri/2024",
+  title: "Galeri HIMASI 2024",
+  desc: "Dokumentasi kegiatan HIMASI UNAS tahun 2024",
+  images: galleryImages2024,
+};
