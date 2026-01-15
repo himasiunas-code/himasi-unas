27572ed918d958a083dc6a2ea8796c790dcda00f
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-01-15 18:48:32 +0700

    fix: update banner image container to use aspect ratio for better responsiveness

--- components/shared/Home/Activity.tsx
+++ components/shared/Home/Activity.tsx
@@ -15,7 +15,7 @@ export default function Activity() {
                         <div className="flex gap-4 md:gap-6 pb-4" style={{ width: 'max-content' }}>
                             {[...BANNER_DATA].reverse().map((banner) => (
                                 <div key={banner.id} className="group relative overflow-hidden rounded-xl shadow-xl hover:shadow-2xl transition-all duration-300 w-72 sm:w-80 lg:w-96 shrink-0">
-                                    <div className="relative h-44 sm:h-48 lg:h-64 overflow-hidden">
+                                    <div className="relative aspect-video overflow-hidden">
                                         <Image
                                             src={banner.image}
                                             alt={banner.alt}
