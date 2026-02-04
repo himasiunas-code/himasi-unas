6314fb3d286bd14bd930fa3424b63f88b4c3159e
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-02-05 00:38:15 +0700

    fix: comment out Feed component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <Introduction />
             <Slide />
             <Kegiatan />
-            <Feed />
+            {/* <Feed /> */}
         </main>
     )
 }
