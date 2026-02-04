71a7cb0c1386f142e8d0180c894a28b9c164f369
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-02-04 23:48:48 +0700

    feat: add Feed component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/shared/Home/Hero";
 import Introduction from "@/components/shared/Home/Introduction";
 import Slide from "@/components/shared/Home/SlideImage";
 import Kegiatan from "@/components/shared/Home/Activity";
+import Feed from "@/components/shared/Home/Berita";
 
 export default function Home() {
     return(
@@ -10,6 +11,7 @@ export default function Home() {
             <Introduction />
             <Slide />
             <Kegiatan />
+            <Feed />
         </main>
     )
 }
