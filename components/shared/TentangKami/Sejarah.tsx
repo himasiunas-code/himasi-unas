a61e8939c6c095f804b0d256b084ec855b016113
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-02-12 11:30:57 +0700

    fix: update image paths for ketua data in Sejarah component

--- components/shared/TentangKami/Sejarah.tsx
+++ components/shared/TentangKami/Sejarah.tsx
@@ -10,43 +10,43 @@ export default function Sejarah() {
       periode: "2024/2025",
       ketua: "Rayhan Vernandhi",
       wakil: "Muhammad Ichsan",
-      image: "/image/Tentang-kami/Kahim/2025.jpeg"
+      image: "/image/Tentang-kami/Kahim/2025.png"
     },
     {
       periode: "2023/2024",
       ketua: "Difa Nur Atsalis",
       wakil: "Baldhan Difa",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2024.png"
     },
     {
       periode: "2022/2023",
       ketua: "Fathur Rahman Riffandy",
       wakil: "Muhammad Bayu Anggara",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2023.png"
     },
     {
       periode: "2021/2022",
       ketua: "Ilham Rachmandito",
       wakil: "Muhammad Farhan",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2022.png"
     },
     {
       periode: "2020/2021",
       ketua: "Della Diniyanti",
       wakil: "Fatha Alsidqi",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2021.png"
     },
     {
       periode: "2019/2020",
       ketua: "M. Alwi saepul Z",
       wakil: "Indrawan",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2020.png"
     },
     {
       periode: "2017/2018",
       ketua: "Irman Mega Firdaus",
       wakil: "Erlal Anggara",
-      image: "/Coming.jpg"
+      image: "/image/Tentang-kami/Kahim/2018.png"
     }
   ];
 
