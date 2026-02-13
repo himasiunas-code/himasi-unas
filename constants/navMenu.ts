31bd466e8995e31cfa433fe0300a321fd039eed1
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-02-14 00:08:08 +0700

    fix: update gallery paths in navMenu for correct year ranges

--- constants/navMenu.ts
+++ constants/navMenu.ts
@@ -14,11 +14,11 @@ export const navMenu = [
     title: "Galeri",
     dropdown: [
       {
-        path: "/galeri/2025",
+        path: "/galeri/2025-2026",
         title: "2025/2026",
       },
       {
-        path: "/galeri/2024",
+        path: "/galeri/2024-2025",
         title: "2024/2025",
       },
     ],
