a6d4fa5fa99b21845dd02bc01514e038e6380a8a
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-04-17 11:33:12 +0700

    fix: update base URL in sitemap to include 'www'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://himasiunas.com'
+  const baseUrl = 'https://www.himasiunas.com'
 
   return [
     {
