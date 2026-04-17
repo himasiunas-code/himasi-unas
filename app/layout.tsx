1a23dacad386292dde190540d9e9a520784d3136
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-04-17 11:33:05 +0700

    fix: add metadataBase URL to metadata configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.himasiunas.com"),
   title: "HIMASI UNAS",
   description:
     "Himpunan Mahasiswa Sistem Informasi Universitas Nasional. Organisasi mahasiswa yang berdedikasi untuk mengembangkan potensi akademik, profesional, dan sosial di bidang Sistem Informasi. Dengan berbagai kegiatan seperti seminar, pelatihan, hingga proyek kolaboratif, kami bertujuan untuk menciptakan lingkungan belajar yang inspiratif sekaligus membangun jejaring yang solid di antara mahasiswa.",
