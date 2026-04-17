68e3127336bca2002f6ad62c086c8bb8964dbb5d
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-04-17 11:27:57 +0700

    fix: update sitemap URL for galeri to include year segment

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/galeri`,
+      url: `${baseUrl}/galeri/2025-2026`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
