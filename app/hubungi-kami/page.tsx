b636ee689171ae8174204cf7004b16eba58a21ee
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-04-17 11:28:05 +0700

    feat: add metadata for Hubungi Kami page

--- app/hubungi-kami/page.tsx
+++ app/hubungi-kami/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
 import BannerHubungi from "@/components/shared/Hubungi/HeroBanner"; 
 import ContactCard from "@/components/shared/Hubungi/ContactCards";
 import Location from "@/components/shared/Hubungi/LocationSection";
 import ContactForm from "@/components/shared/Hubungi/ContactForm";
+
+export const metadata: Metadata = {
+  title: 'Hubungi Kami | HIMASI UNAS',
+  description: 'Silakan hubungi Himpunan Mahasiswa Sistem Informasi (HIMASI) Universitas Nasional jika Anda memiliki pertanyaan atau ingin bekerja sama.'
+};
 
 export default function PageHubungi() {
   return (
