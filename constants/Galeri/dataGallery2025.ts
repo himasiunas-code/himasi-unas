1a99fc9bd859fc4f11dd710647fbed13d8164bf1
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-04-29 19:09:23 +0700

    add image

--- constants/Galeri/dataGallery2025.ts
+++ constants/Galeri/dataGallery2025.ts
@@ -1,11 +1,236 @@
 import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri/Galeri";
 
 export const galleryImages2025: GalleryImage[] = [
-//   {
-//     src: "/galeri/2025/foto1.jpg",
-//     alt: "Kegiatan HIMASI 2025 - 1",
-//     category: "PLBA",
-//   },
+  {
+    src: "/image/Galeri/Oprec/IMG_5357.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 1",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5361.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 2",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5364.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 3",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5365.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 4",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5369.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 5",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5370.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 6",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5378.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 7",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5385.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 8",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5386.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 9",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5389.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 10",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5390.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 11",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5400.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 12",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5401.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 13",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5418.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 14",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5428.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 15",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5430.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 16",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5431.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 17",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5450.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 18",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5451.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 19",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5484.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 20",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5493.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 21",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5500.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 22",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5506.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 23",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5524.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 24",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5525.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 25",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5534.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 26",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5535.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 27",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5539.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 28",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5540.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 29",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5564.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 30",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5977.JPG",
+    alt: "PLBA HIMASI 2025 - 1",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5980.JPG",
+    alt: "PLBA HIMASI 2025 - 2",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5981.JPG",
+    alt: "PLBA HIMASI 2025 - 3",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5982.JPG",
+    alt: "PLBA HIMASI 2025 - 4",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5983.JPG",
+    alt: "PLBA HIMASI 2025 - 5",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5984.JPG",
+    alt: "PLBA HIMASI 2025 - 6",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5985.JPG",
+    alt: "PLBA HIMASI 2025 - 7",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5986.JPG",
+    alt: "PLBA HIMASI 2025 - 8",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5987.JPG",
+    alt: "PLBA HIMASI 2025 - 9",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5988.JPG",
+    alt: "PLBA HIMASI 2025 - 10",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5989.JPG",
+    alt: "PLBA HIMASI 2025 - 11",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5990.JPG",
+    alt: "PLBA HIMASI 2025 - 12",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5991.JPG",
+    alt: "PLBA HIMASI 2025 - 13",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5992.JPG",
+    alt: "PLBA HIMASI 2025 - 14",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5993.JPG",
+    alt: "PLBA HIMASI 2025 - 15",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5994.JPG",
+    alt: "PLBA HIMASI 2025 - 16",
+    category: "PLBA",
+  },
 ];
 
 export const galleryEvent2025: GalleryEvent = {
