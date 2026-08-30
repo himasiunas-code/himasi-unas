e052f208a07ed10d61c51545a5ae7decd622e421
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:28:51 +0700

    feat: implement Home page constants, type definitions, and shared components for vision, mission, and divisions

---
+++ components/shared/Home/index.ts
@@ -0,0 +1,6 @@
+// Export seluruh komponen modul Home
+export { default as HomeActivity } from "./HomeActivity";
+export { default as HomeBerita } from "./HomeBerita";
+export { default as HomeHero } from "./HomeHero";
+export { default as HomeIntroduction } from "./HomeIntroduction";
+export { default as HomeSlideImage } from "./HomeSlideImage";
