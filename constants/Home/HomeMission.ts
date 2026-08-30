a432842d825d9200379c89e0f6673a2d275b3d1c
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:29:27 +0700

    feat: initialize Home module types and constants with mission, banner, division, and slider data structures

---
+++ constants/Home/HomeMission.ts
@@ -0,0 +1,21 @@
+import { Mission } from "@/lib/type/Home/HomeMission";
+
+// Data poin-poin misi HIMASI
+export const MISSION_DATA: Mission[] = [
+  {
+    id: 1,
+    text: "Meningkatkan eksistensi dan keterlibatan mahasiswa",
+  },
+  {
+    id: 2,
+    text: "Memperkuat branding HIMASI di kampus",
+  },
+  {
+    id: 3,
+    text: "Mendorong kolaborasi internal dan eksternal",
+  },
+  {
+    id: 4,
+    text: "Menciptakan HIMASI yang humanis dan adaptif",
+  },
+];
