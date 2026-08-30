921f69297a53baf1758e77724b66064009c8a69a
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-31 00:13:02 +0700

    feat: implement structure page components and data models for BPH and divisions

---
+++ components/shared/Struktur/index.ts
@@ -0,0 +1,5 @@
+// Export seluruh komponen modul Struktur
+export { default as StrukturBPH } from "./StrukturBPH";
+export { default as StrukturDivision } from "./StrukturDivision";
+export { default as StrukturCircularGallery } from "./StrukturCircularGallery";
+export { default as StrukturSlideGalleryMobile } from "./StrukturSlideGalleryMobile";
