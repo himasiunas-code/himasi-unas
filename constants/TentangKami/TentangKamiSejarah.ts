17fb4e796626840074ff9e380da42fbd881ff842
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-31 00:46:24 +0700

    feat: add leadership history data types and constant records for Tentang Kami module

---
+++ constants/TentangKami/TentangKamiSejarah.ts
@@ -0,0 +1,47 @@
+import { LeaderHistory } from "@/lib/type/TentangKami";
+
+// Riwayat Ketua dan Wakil Ketua HIMASI UNAS dari masa ke masa
+export const ketuaData: LeaderHistory[] = [
+  {
+    periode: "2024/2025",
+    ketua: "Rayhan Vernandhi",
+    wakil: "Muhammad Ichsan",
+    image: "/image/Tentang-kami/Kahim/2025.png",
+  },
+  {
+    periode: "2023/2024",
+    ketua: "Difa Nur Atsalis",
+    wakil: "Baldhan Difa",
+    image: "/image/Tentang-kami/Kahim/2024.png",
+  },
+  {
+    periode: "2022/2023",
+    ketua: "Fathur Rahman Riffandy",
+    wakil: "Muhammad Bayu Anggara",
+    image: "/image/Tentang-kami/Kahim/2023.png",
+  },
+  {
+    periode: "2021/2022",
+    ketua: "Ilham Rachmandito",
+    wakil: "Muhammad Farhan",
+    image: "/image/Tentang-kami/Kahim/2022.png",
+  },
+  {
+    periode: "2020/2021",
+    ketua: "Della Diniyanti",
+    wakil: "Fatha Alsidqi",
+    image: "/image/Tentang-kami/Kahim/2021.png",
+  },
+  {
+    periode: "2019/2020",
+    ketua: "M. Alwi saepul Z",
+    wakil: "Indrawan",
+    image: "/image/Tentang-kami/Kahim/2020.png",
+  },
+  {
+    periode: "2017/2018",
+    ketua: "Irman Mega Firdaus",
+    wakil: "Erlal Anggara",
+    image: "/image/Tentang-kami/Kahim/2018.png",
+  },
+];
