061e90a5800b998e1e5ec2556d057517db714904
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:29:14 +0700

    feat: add constants and types for home page sections including banner, divisions, missions, and sliders

---
+++ constants/Home/HomeBanner.ts
@@ -0,0 +1,76 @@
+import { TBanner } from "@/lib/type/Home/HomeBanner";
+
+// Data banner kegiatan utama yang ditampilkan di halaman beranda
+export const BANNER_DATA: TBanner[] = [
+  {
+    id: 1,
+    title: "Studi Banding",
+    description:"HIMASI UNAS × HMSI ITK",
+    image: "image/Home/Kegiatan/HIMASI UNAS × HMSI ITK.png",
+    alt: "HIMASI UNAS × HMSI ITK",
+  },
+  {
+    id: 2,
+    title: "Company Visit",
+    description: "Company Visit Goes To PT. Torabika Eka Semesta",
+    image: "image/Home/Kegiatan/Company Visit Goes To PT. Torabika Eka Semesta.png",
+    alt: "Company Visit Goes To PT. Torabika Eka Semesta",
+  },
+  {
+    id: 3,
+    title: "Workshop",
+    description: "Peningkatan Business Intelligence pada Platform Google Colab Menggunakan Bahasa Python",
+    image: "image/Home/Kegiatan/Peningkatan Business Intelligence pada Platform Google Colab Menggunakan Bahasa Python.jpg",
+    alt: "Peningkatan Business Intelligence pada Platform Google Colab Menggunakan Bahasa Python",
+  },
+  {
+    id: 4,
+    title: "CERMASI Podcast",
+    description: "Business Intelligence Susah Itu Susah? Gak Juga",
+    image: "image/Home/Kegiatan/Business Intelligence Susah Itu Susah, Gak Juga.png",
+    alt: "Business Intelligence Susah Itu Susah? Gak Juga",
+  },
+  {
+    id: 5,
+    title: "Company Visit",
+    description: "HIMASI ke PT Sari Teknologi “Bridging Campus and Industry”",
+    image: "image/Home/Kegiatan/Robopark.jpg",
+    alt: "HIMASI ke PT Sari Teknologi “Bridging Campus and Industry”",
+  },
+  {
+    id: 6,
+    title: "International Student Mobility",
+    description: "Student Mobility to University College Sedaya International (UCSI) Malaysia",
+    image: "image/Home/Kegiatan/Student mobility to University College Sedaya International.png",
+    alt: "Student mobility to University College Sedaya International",
+  },
+  {
+    id: 7,
+    title: "International Benchmarking",
+    description: "International Benchmarking at University Poly-Tech Malaysia (UPTM)",
+    image: "image/Home/Kegiatan/International Benchmarking at Universiti Poly-Tech Malaysia.png",
+    alt: "International Benchmarking at University Poly-Tech Malaysia",
+  },
+  {
+    id: 8,
+    title: "International PKM",
+    description: "International PKM  to the Guidance Center for Children of Indonesian Migrant Workers",
+    image: "image/Home/Kegiatan/International PKM  to the Guidance Center for Children of Indonesian Migrant Workers.jpeg",
+    alt: "International PKM  to the Guidance Center for Children of Indonesian Migrant Workers",
+  },
+  {
+    id: 9,
+    title: "International Visit",
+    description: "Visit the Embassy of the Republic of Indonesia in Kuala Lumpur, Malaysia",
+    image: "image/Home/Kegiatan/Embassy of the Republic of Indonesia in Kuala Lumpur.png",
+    alt: "Visit the Embassy of the Republic of Indonesia in Kuala Lumpur",
+  },
+  {
+    id: 10,
+    title: "Workshop",
+    description:
+      "Workshop Game Developer untuk meningkatkan keterampilan dalam pengembangan game.",
+    image: "image/Home/Kegiatan/workshop-game-developer.png",
+    alt: "Workshop Game Developer HIMASI UNAS",
+  },
+];
