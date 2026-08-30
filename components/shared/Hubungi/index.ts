38d2c198c58f87c129ff8e3305284ec46289f476
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-31 00:50:32 +0700

    feat: implement Hubungi Kami component module with supporting types and data constants

---
+++ components/shared/Hubungi/index.ts
@@ -0,0 +1,5 @@
+// Export seluruh komponen modul Hubungi Kami
+export { default as HubungiHero } from "./HubungiHero";
+export { default as HubungiContactCards } from "./HubungiContactCards";
+export { default as HubungiLocation } from "./HubungiLocation";
+export { default as HubungiContactForm } from "./HubungiContactForm";
