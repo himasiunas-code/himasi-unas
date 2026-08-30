c47b77f42a94e4c266f86744ecbee22c663dc87b
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:29:33 +0700

    feat: implement home page domain models and image slide constants

---
+++ constants/Home/HomeSlideImages.ts
@@ -0,0 +1,53 @@
+import BPH from "@/public/image/Home/BPH.png";
+import RND from "@/public/image/Home/RND.png";
+import PR from "@/public/image/Home/PR.png";
+import CM from "@/public/image/Home/CM.png";
+import ETR from "@/public/image/Home/ETR.png";
+import HTD from "@/public/image/Home/HTD.png";
+import { SlideImageData } from "@/lib/type/Home/HomeSlideImage";
+
+// Data foto dan kartu untuk slider berputar di halaman beranda
+export const slideImages: SlideImageData[] = [
+  {
+    id: 1,
+    src: BPH,
+    alt: "HIMASI Keren",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 2,
+    src: RND,
+    alt: "HIMASI Hebat",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 3,
+    src: PR,
+    alt: "HIMASI Kuat",
+    backgroundColor: "#ffffffff",
+    textColor: "black",
+  },
+  {
+    id: 4,
+    src: CM,
+    alt: "HIMASI Kreatif",
+    backgroundColor: "#ffffffff",
+    textColor: "black",
+  },
+  {
+    id: 5,
+    src: ETR,
+    alt: "HIMASI Ramah",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+  {
+    id: 6,
+    src: HTD,
+    alt: "HIMASI UNAS",
+    backgroundColor: "#ffffffff",
+    textColor: "white",
+  },
+];
