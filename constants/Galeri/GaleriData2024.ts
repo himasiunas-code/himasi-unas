8a9a9c81c5bf13c9e60ba6436516269625e71d2a
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:54:26 +0700

    feat: add gallery data and filter constants for 2024 and 2025 with supporting type definitions

---
+++ constants/Galeri/GaleriData2024.ts
@@ -0,0 +1,272 @@
+import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri";
+
+// Koleksi foto kegiatan HIMASI tahun akademik 2024/2025
+export const galleryImages2024: GalleryImage[] = [
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00489.JPG",
+    alt: "PLBA HIMASI 2024 - 1",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00490.JPG",
+    alt: "PLBA HIMASI 2024 - 2",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00491.JPG",
+    alt: "PLBA HIMASI 2024 - 3",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00492.JPG",
+    alt: "PLBA HIMASI 2024 - 4",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00493.JPG",
+    alt: "PLBA HIMASI 2024 - 5",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00495.JPG",
+    alt: "PLBA HIMASI 2024 - 6",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00497.JPG",
+    alt: "PLBA HIMASI 2024 - 7",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00498.JPG",
+    alt: "PLBA HIMASI 2024 - 8",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00499.JPG",
+    alt: "PLBA HIMASI 2024 - 9",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/PLBA/DSC00500.JPG",
+    alt: "PLBA HIMASI 2024 - 10",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/20250526_102418.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 1",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05514.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 2",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05526.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 3",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05546.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 4",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05552.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 5",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05572.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 6",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05574.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 7",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05585.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 8",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05604.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 9",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05614.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 10",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/DSC05626.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 11",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Mayora/IMG-20250526-WA0042.jpg",
+    alt: "Company Visit Mayora HIMASI 2024 - 12",
+    category: "Mayora",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05651.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 1",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05652.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 2",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05653.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 3",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05654.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 4",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05655.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 5",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05656.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 6",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05657.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 7",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05658.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 8",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05659.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 9",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05660.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 10",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05661.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 11",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05662.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 12",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05664.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 13",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05665.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 14",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05666.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 15",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05669.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 16",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05670.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 17",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05671.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 18",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05672.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 19",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Goes Malaysia/DSC05673.JPG",
+    alt: "Goes to Malaysia HIMASI 2024 - 20",
+    category: "Goes to Malaysia",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8134.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 1",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8135.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 2",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8136.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 3",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8137.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 4",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8138.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 5",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8139.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 6",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8142.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 7",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8143.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 8",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8144.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 9",
+    category: "Stuban ITK",
+  },
+  {
+    src: "/image/Galeri/2024/Stuban ITK/_DSC8145.JPG",
+    alt: "Stuban ITK Kalimantan HIMASI 2024 - 10",
+    category: "Stuban ITK",
+  },
+];
+
+export const galleryEvent2024: GalleryEvent = {
+  path: "galeri/2024",
+  title: "Galeri HIMASI 2024",
+  desc: "Dokumentasi kegiatan HIMASI UNAS tahun 2024",
+  images: galleryImages2024,
+};
