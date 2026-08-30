7b2403ddcc115bd0980f47eeb9083a2d24faa124
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:54:31 +0700

    feat: add 2025 gallery data and standardize type definitions for gallery modules

---
+++ constants/Galeri/GaleriData2025.ts
@@ -0,0 +1,452 @@
+import { GalleryImage, GalleryEvent } from "@/lib/type/Galeri";
+
+// Koleksi foto kegiatan HIMASI tahun akademik 2025/2026
+export const galleryImages2025: GalleryImage[] = [
+  {
+    src: "/image/Galeri/Oprec/IMG_5357.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 1",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5361.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 2",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5364.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 3",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5365.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 4",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5369.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 5",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5370.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 6",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5378.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 7",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5385.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 8",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5386.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 9",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5389.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 10",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5390.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 11",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5400.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 12",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5401.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 13",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5418.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 14",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5428.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 15",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5430.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 16",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5431.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 17",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5450.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 18",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5451.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 19",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5484.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 20",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5493.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 21",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5500.JPG",
+    alt: "Open Rekrutmen HIMASI 2025 - 22",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5506.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 23",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5524.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 24",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5525.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 25",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5534.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 26",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5535.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 27",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5539.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 28",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5540.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 29",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/Oprec/IMG_5564.jpg",
+    alt: "Open Rekrutmen HIMASI 2025 - 30",
+    category: "Himpunan",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5977.JPG",
+    alt: "PLBA HIMASI 2025 - 1",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5980.JPG",
+    alt: "PLBA HIMASI 2025 - 2",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5981.JPG",
+    alt: "PLBA HIMASI 2025 - 3",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5982.JPG",
+    alt: "PLBA HIMASI 2025 - 4",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5983.JPG",
+    alt: "PLBA HIMASI 2025 - 5",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5984.JPG",
+    alt: "PLBA HIMASI 2025 - 6",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5985.JPG",
+    alt: "PLBA HIMASI 2025 - 7",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5986.JPG",
+    alt: "PLBA HIMASI 2025 - 8",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5987.JPG",
+    alt: "PLBA HIMASI 2025 - 9",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5988.JPG",
+    alt: "PLBA HIMASI 2025 - 10",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5989.JPG",
+    alt: "PLBA HIMASI 2025 - 11",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5990.JPG",
+    alt: "PLBA HIMASI 2025 - 12",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5991.JPG",
+    alt: "PLBA HIMASI 2025 - 13",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5992.JPG",
+    alt: "PLBA HIMASI 2025 - 14",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5993.JPG",
+    alt: "PLBA HIMASI 2025 - 15",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/PLBA/IMG_5994.JPG",
+    alt: "PLBA HIMASI 2025 - 16",
+    category: "PLBA",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103159.jpg",
+    alt: "CERMASI HIMASI 2025 - 1",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103214.jpg",
+    alt: "CERMASI HIMASI 2025 - 2",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103242.jpg",
+    alt: "CERMASI HIMASI 2025 - 3",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103321.jpg",
+    alt: "CERMASI HIMASI 2025 - 4",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103354.jpg",
+    alt: "CERMASI HIMASI 2025 - 5",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103431.jpg",
+    alt: "CERMASI HIMASI 2025 - 6",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103451.jpg",
+    alt: "CERMASI HIMASI 2025 - 7",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103516.jpg",
+    alt: "CERMASI HIMASI 2025 - 8",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103533.jpg",
+    alt: "CERMASI HIMASI 2025 - 9",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103542.jpg",
+    alt: "CERMASI HIMASI 2025 - 10",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103544.jpg",
+    alt: "CERMASI HIMASI 2025 - 11",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103633.jpg",
+    alt: "CERMASI HIMASI 2025 - 12",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103804.jpg",
+    alt: "CERMASI HIMASI 2025 - 13",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_103939.jpg",
+    alt: "CERMASI HIMASI 2025 - 14",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_104057.jpg",
+    alt: "CERMASI HIMASI 2025 - 15",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_104305.jpg",
+    alt: "CERMASI HIMASI 2025 - 16",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_105248.jpg",
+    alt: "CERMASI HIMASI 2025 - 17",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135559.jpg",
+    alt: "CERMASI HIMASI 2025 - 18",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135623.jpg",
+    alt: "CERMASI HIMASI 2025 - 19",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135637.jpg",
+    alt: "CERMASI HIMASI 2025 - 20",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135638.jpg",
+    alt: "CERMASI HIMASI 2025 - 21",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135640.jpg",
+    alt: "CERMASI HIMASI 2025 - 22",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135823.jpg",
+    alt: "CERMASI HIMASI 2025 - 23",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135846.jpg",
+    alt: "CERMASI HIMASI 2025 - 24",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135849.jpg",
+    alt: "CERMASI HIMASI 2025 - 25",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135924.jpg",
+    alt: "CERMASI HIMASI 2025 - 26",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_135931.jpg",
+    alt: "CERMASI HIMASI 2025 - 27",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140002.jpg",
+    alt: "CERMASI HIMASI 2025 - 28",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140020.jpg",
+    alt: "CERMASI HIMASI 2025 - 29",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140056.jpg",
+    alt: "CERMASI HIMASI 2025 - 30",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140114.jpg",
+    alt: "CERMASI HIMASI 2025 - 31",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140200.jpg",
+    alt: "CERMASI HIMASI 2025 - 32",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140556.jpg",
+    alt: "CERMASI HIMASI 2025 - 33",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140558.jpg",
+    alt: "CERMASI HIMASI 2025 - 34",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140635.jpg",
+    alt: "CERMASI HIMASI 2025 - 35",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/Cermasi/20260221_140640.jpg",
+    alt: "CERMASI HIMASI 2025 - 36",
+    category: "CERMASI",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/3a34b05c-c5b7-4074-97c0-2be2e6d3c079.jpg",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 1",
+    category: "Seminar Kolab",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/5eea72bd-5529-4fbd-8f3c-4381575b8209.jpg",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 2",
+    category: "Seminar Kolab",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/c1fade5c-b1d8-4a73-9319-a90b704bd729.jpg",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 3",
+    category: "Seminar Kolab",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/f23fb6b3-f06c-41fb-ad95-7cf0aab0b11e.jpg",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 4",
+    category: "Seminar Kolab",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/IMG_1872.JPG",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 5",
+    category: "Seminar Kolab",
+  },
+  {
+    src: "/image/Galeri/2025/Seminar Kolab/IMG_1874.JPG",
+    alt: "Seminar Kolaborasi AMD Ryzen AI HIMASI 2025 - 6",
+    category: "Seminar Kolab",
+  },
+];
+
+export const galleryEvent2025: GalleryEvent = {
+  path: "galeri/2025",
+  title: "Galeri HIMASI 2025",
+  desc: "Dokumentasi kegiatan HIMASI UNAS tahun 2025",
+  images: galleryImages2025,
+};
