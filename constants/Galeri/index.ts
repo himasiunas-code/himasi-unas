01dfbf42a5bae039e32e9be2f46d4826ded9bde5
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:54:50 +0700

    feat: define TypeScript interfaces and constants for gallery data modules

---
+++ constants/Galeri/index.ts
@@ -0,0 +1,5 @@
+// Export seluruh data dan filter modul Galeri
+export * from "./GaleriData2024";
+export * from "./GaleriData2025";
+export * from "./GaleriFilter2024";
+export * from "./GaleriFilter2025";
