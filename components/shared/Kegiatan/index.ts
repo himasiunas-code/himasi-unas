eda08d1fcd55da23bac82f3259ca8a6caab69b12
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:43:26 +0700

    feat: implement KegiatanPendaftaran component and related activity data structures

---
+++ components/shared/Kegiatan/index.ts
@@ -0,0 +1,4 @@
+// Export seluruh komponen modul Kegiatan
+export { default as KegiatanHero } from "./KegiatanHero";
+export { default as KegiatanPendaftaran } from "./KegiatanPendaftaran";
+export { default as KegiatanArtikel } from "./KegiatanArtikel";
