e81e7169263b525a93e348e703e3048fb563f688
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-30 23:54:39 +0700

    feat: add 2024 and 2025 gallery filter constants and type definitions

---
+++ constants/Galeri/GaleriFilter2024.ts
@@ -0,0 +1,46 @@
+import { GalleryFilter } from "@/lib/type/Galeri";
+
+// Kategori filter kegiatan 2024
+export const galleryFilters2024: GalleryFilter[] = [
+  "All",
+  "PLBA",
+  "Mayora",
+  "Goes to Malaysia",
+  "Stuban ITK",
+];
+
+// Keterangan dan nama display filter kegiatan 2024
+export const filterDescriptions2024: Record<
+  GalleryFilter,
+  { displayName: string; description: string }
+> = {
+  All: { displayName: "All", description: "Semua Foto Kegiatan 2024" },
+  "PLBA": {
+    displayName: "PLBA",
+    description: "Pengenalan Lingkungan dan Budaya Akademik 2024",
+  },
+  Himpunan: {
+    displayName: "Himpunan",
+    description: "Kebersamaan HIMASI 2024",
+  },
+  Mayora: {
+    displayName: "Mayora",
+    description: "Company Visit Mayora 2024",
+  },
+  "Goes to Malaysia": {
+    displayName: "Goes to Malaysia",
+    description: "Goes to Malaysia 2024",
+  },
+  "Stuban ITK": {
+    displayName: "Stuban ITK",
+    description: "Stuban ITK Kalimantan 2024",
+  },
+  CERMASI: {
+    displayName: "CERMASI",
+    description: "CERMASI HIMASI 2024",
+  },
+  "Seminar Kolab": {
+    displayName: "Seminar Kolab",
+    description: "Seminar Kolaborasi AMD Ryzen AI 2024",
+  },
+};
