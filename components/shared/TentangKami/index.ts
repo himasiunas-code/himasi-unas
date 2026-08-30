6fa296bafb9b771d2e4b09af1ce7ea531fc7d6c1
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-08-31 00:45:43 +0700

    feat: implement TentangKami module featuring vision, mission, and leadership history components

---
+++ components/shared/TentangKami/index.ts
@@ -0,0 +1,5 @@
+// Export seluruh komponen modul Tentang Kami
+export { default as TentangKamiHero } from "./TentangKamiHero";
+export { default as TentangKamiVM } from "./TentangKamiVM";
+export { default as TentangKamiFilosofi } from "./TentangKamiFilosofi";
+export { default as TentangKamiSejarah } from "./TentangKamiSejarah";
