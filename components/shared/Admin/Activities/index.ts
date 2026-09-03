bad9524e1176666f378ff1e2aea67ce1dcab9876
CatwisBot <whisnuaryanugraha04@gmail.com>
2026-09-03 22:55:33 +0700

    feat: implement comprehensive admin dashboard components including statistics, registration management, and authentication modules

---
+++ components/shared/Admin/Activities/index.ts
@@ -0,0 +1,6 @@
+// Re-export seluruh modul komponen Manajemen Kegiatan Admin
+export { default as ActivitiesHeader } from './ActivitiesHeader'
+export { default as ActivityFormDialog } from './ActivityFormDialog'
+export { default as ActivityCard } from './ActivityCard'
+export { default as ActivitiesList } from './ActivitiesList'
+export * from './types'
